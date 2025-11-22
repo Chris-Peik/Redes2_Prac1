7bf61d3ab3df2cab332f238ad6c581f666eb5c93
Chris-Peik <cnunezr1500@alumno.ipn.mx>
2025-11-22 16:38:06 -0600

    LOL

--- src/Cliente.java
+++ src/Cliente.java
@@ -12,29 +12,50 @@ public class Cliente {
             System.out.println(host);
             
             Socket cl = new Socket(host,pto);
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(cl.getInputStream(),"ISO-8859-1"));
+            
             System.out.println("Conexion con el servidor "+dir+":"+pto+" establecida\n");
             PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream(),"ISO-8859-1"));
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in,"ISO-8859-1"));//"Windows-1250"
-            BufferedReader br1 = new BufferedReader(new InputStreamReader(cl.getInputStream(),"ISO-8859-1"));
-
+            
+            imprScrn(0);
+            String mensaje = br.readLine();  //Integer.MAX_VALUE
+            System.out.println(mensaje);
             while(true){
+                
                 System.out.println("Escribe un mensaje, <ENTER> para enviar, \"salir\" para terminar");
-                String mensaje = br.readLine();  //Integer.MAX_VALUE
-                System.out.println("LOL");
-                pw.println(mensaje);
+                String mensaje2 = br.readLine();  //Integer.MAX_VALUE
+                pw.println(mensaje2);
                 pw.flush();
-                if(mensaje.compareToIgnoreCase("salir")==0){
+                if(mensaje2.compareToIgnoreCase("salir")==0){
                     br.close();
                     pw.close();
                     cl.close();
                     System.exit(0);
                 } else{
-                    String eco = br1.readLine();
-                    System.out.println("Eco recibido desde "+cl.getInetAddress()+":"+cl.getPort()+" "+eco);
+                    String eco2 = br1.readLine();
+                    System.out.println("Eco recibido desde "+cl.getInetAddress()+":"+cl.getPort()+" "+eco2);
                 }//else*/
             }
         } catch (Exception e) {
             // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void imprScrn(int key){
+        switch (key) {
+            case 0:
+                System.out.println("         FARMACIAS          \n" + //
+                                        "         PARECIDAS          \n" + //
+                                        "                            \n" + //
+                                        "CASI LO MISMO, NO TAN BARATO\n" + //
+                                        "                            \n" + //
+                                        "       PRESIONA ENTER       ");
+                break;
+        
+            default:
+                break;
         }
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,5 +1,8 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 public class Servidor {
     public static void main(String[] args) throws Exception {
@@ -13,6 +16,9 @@ public class Servidor {
                 System.out.println("Cliente conectado desde "+cl.getInetAddress()+":"+cl.getPort());
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream(),"ISO-8859-1"));
                 BufferedReader br = new BufferedReader(new InputStreamReader(cl.getInputStream(),"ISO-8859-1"));
+                String catalogo=leerJSON();
+                System.out.println("Enviando catalogo a "+cl.getInetAddress()+":"+cl.getPort());
+                pw.println(catalogo);
                 while(true){
                     String msj = br.readLine(); //  \n\r (10)(13)
                     if(msj.compareToIgnoreCase("salir")==0){
@@ -23,6 +29,7 @@ public class Servidor {
                         break;
                     } else{
                         System.out.println("Mensaje recibido: "+msj+" devolviendo eco");
+                        msj=leerJSON();
                         pw.println(msj);
                         pw.flush();
                     }//else
@@ -32,5 +39,31 @@ public class Servidor {
             // TODO: handle exception
             System.out.println("xd");
         }
+    }
+    public static String leerJSON() {
+
+        try {
+            File file = new File("catalogo.json");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder jsonContent = new StringBuilder();
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                jsonContent.append(linea);
+                
+            }
+            
+            reader.close();
+            
+            String json = jsonContent.toString();
+            return json;
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return e.getMessage();
+        }
+
+
+
     }
 }
